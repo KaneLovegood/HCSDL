f4893f369b627b7bedad792b25257f2678a7dd8e
Khang Lê <131420407+KaneLovegood@users.noreply.github.com>
2023-11-09 11:21:25 +0700

    Add files via upload

---
+++ Tuan10.sql
@@ -0,0 +1,49 @@
+﻿--1.  Tạo view  vw_Products_Info  hiển thị danh sách các sản phẩm từ bảng 
+--Products  và  bảng  Categories.  Thông  tin  bao  gồm  CategoryName, 
+--Description,  ProductName,  QuantityPerUnit,  UnitPrice,  UnitsInStock  ? 
+--Thực hiện truy vấn dữ liệu từ View ?
+
+create view vw_Products_Info
+as 
+SELECT        Products.QuantityPerUnit, Categories.CategoryName, Categories.Description, Products.ProductName, Products.UnitPrice, Products.UnitsInStock
+FROM            Categories INNER JOIN
+                         Products ON Categories.CategoryID = Products.CategoryID
+
+select * from vw_Products_Info
+--2.  Tạo view  vw_CustomerTotals  hiển thị tổng tiền  các hóa đơn của  mỗi 
+--khách  hàng  theo  tháng  và  theo  năm.  Thông  tin  gồm  CustomerID, 
+--YEAR(OrderDate)  AS  Year,  MONTH(OrderDate)  AS  Month, 
+--SUM(UnitPrice*Quantity) ? 
+
+create view vw_CustomerTotals
+as 
+SELECT        Customers.CustomerID,month( Orders.OrderDate) as "month", year(Orders.OrderDate) AS "Year",sum( [Order Details].Quantity *[Order Details].UnitPrice ) as Total
+FROM            Customers INNER JOIN
+                         Orders ON Customers.CustomerID = Orders.CustomerID INNER JOIN
+                         [Order Details] ON Orders.OrderID = [Order Details].OrderID
+						 group by Customers.CustomerID, month( Orders.OrderDate), year(Orders.OrderDate)
+
+--Xem lại cú pháp lệnh tạo view ? Thực hiện truy vấn dữ liệu từ View ?
+select * from vw_CustomerTotals
+--3.  Tạo view  hiển thị  tổng số lượng sản phẩm bán được của mỗi nhân viên 
+--theo  từng  năm.  Thông  tin  gồm  EmployeeID,  OrderYear, 
+--SumOfQuantity. Yêu cầu : người dùng không xem được cú pháp lệnh tạo 
+--view ?
+--Xem lại cú pháp lệnh tạo view ? Thực hiện truy vấn dữ liệu từ View ?
+--4.  Tạo view ListCustomer_view  chứa danh sách các khách hàng có trên 5 
+--hóa đơn đặt hàng từ năm 1997 đến 1998,  thông tin gồm mã khách  hàng
+--(CustomerID) , họ tên (CompanyName), Số hóa đơn (CountOfOrders) ?
+--Thực hiện truy vấn dữ liệu từ View ?
+--5.  Tạo  view  ListProduct_view  chứa  danh  sách  những  sản  phẩm  thuộc
+--nhóm  hàng  ‘Beverages’  và  ‘Seafood’  có  tổng  số  lượng  bán  trong  mỗi 
+--năm  trên  30  sản  phẩm,  thông  tin  gồm  CategoryName,  ProductName, 
+--Year, SumOfQuantity ?
+--Thực hiện truy vấn dữ liệu từ View ?
+--6.  Tạo view  vw_OrderSummary  với từ khóa WITH ENCRYPTION gồm 
+--OrderYear (năm của ngày lập hóa đơn), OrderMonth (tháng của ngày lập 
+--hóa đơn), OrderTotal (tổng tiền sum(UnitPrice*Quantity)) ?
+--Thực hiện truy vấn dữ liệu từ View ?
+--Viết lệnh để thấy công dụng của từ khóa trên ?
+--7.  Tạo  view  vwProducts  với  từ  khóa  WITH  SCHEMABINDING  gồm 
+--ProductID, ProductName, Discount ?
+--Thực hiện truy vấn dữ liệu từ View ?
---
+++ Tuan9.sql
@@ -0,0 +1,24 @@
+﻿--a.  Insert  dữ liệu vào bảng KhachHang trong QLBH  với dữ liệu nguồn là 
+--bảng Customers trong NorthWind.
+
+insert into KhachHang (MaKh, TenKh, DiaChi, Phone, SoFax)
+select c.CustomerID, c.ContactName, c.Address, c.Phone, c.Fax 
+from Northwind.dbo.Customers c
+
+--b.  Insert dữ liệu vào  bảng Sanpham trong QLBH. Dữ liệu nguồn là các 
+--sản  phẩm  có  SupplierID  từ  4  đến  29  trong  bảng 
+--Northwind.dbo.Products 
+insert into NhaCungCap
+select s.SupplierID, s.CompanyName, s.Address, s.Phone, s.Fax, s.
+from Northwind.dbo.Suppliers s
+
+insert into SanPham
+select 
+from Northwind.dbo.Products p
+
+--c.  Insert dữ liệu vào  bảng  HoaDon  trong QLBH. Dữ liệu nguồn là  các 
+--hoá  đơn  có  OrderID  nằm  trong  khoảng  10248  đến  10350  trong
+--NorthWind.dbo.[Orders]
+--d.  Insert dữ liệu vào  bảng CT_HoaDon  trong QLBH. Dữ liệu nguồn là
+--các chi tiết hoá đơn có OderID nằm trong khoảng 10248 đến 10350 
+--trong NorthWind.dbo.[Order Detail
